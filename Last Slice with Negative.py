73579617c55d51283d01d9350ccbcb67486df447
Fasiullah Mohammed <36456644+fasiullah-mohammed@users.noreply.github.com>
2022-07-04 18:20:40 -0500

    Add files via upload

---
+++ Last Slice with Negative.py
@@ -0,0 +1,8 @@
+fruits=['Apple', 'Mango', 'grapes', 'banana']
+
+Last_fruits=fruits[2:4]
+
+print('Last Fruits  {}'.format(Last_fruits))
+
+last_fruits=fruits[-2:]
+print('Last Two Fruits   {}'.format(last_fruits))
