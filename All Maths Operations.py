95d1ec3daf4c8345122fe5fb66456d7920f9ae8e
Fasiullah Mohammed <36456644+fasiullah-mohammed@users.noreply.github.com>
2022-07-04 18:07:33 -0500

    Add files via upload

---
+++ All Maths Operations.py
@@ -0,0 +1,13 @@
+sum = 2+3
+Difference = 100-1
+product= 2*4
+power= 2**4
+remainder= 3%2
+divide=8/2
+
+print('Sum: {}'.format(sum))
+print('Diff: {}'.format(Difference))
+print('Multiply: {}'.format(product))
+print('Power: {}'.format(power))
+print('Remainder: {}'.format(remainder))
+print('Divide: {}'.format(divide))
