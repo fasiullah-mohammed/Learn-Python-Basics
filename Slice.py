f8453d84a4c87361fd93fd6cf97ece69d74bdc8d
Fasiullah Mohammed <36456644+fasiullah-mohammed@users.noreply.github.com>
2022-07-04 18:15:38 -0500

    Add files via upload

---
+++ Slice.py
@@ -0,0 +1,11 @@
+fruits=['Apple', 'Mango', 'grapes', 'banana']
+
+few_fruits=fruits[1:3]
+
+print('Some Fruits  {}'.format(few_fruits))
+
+Two_fruits=fruits[0:2]
+print('First Two Fruits   {}'.format(Two_fruits))
+
+Two_again=fruits[:2]
+print('Two Again     {}'.format(Two_again))
