ed975a487f421237c9eff326d106909c589119e3
Fasiullah Mohammed <36456644+fasiullah-mohammed@users.noreply.github.com>
2022-07-18 09:30:48 -0500

    Update WhatsApp Msg.py

--- WhatsApp Msg.py
+++ WhatsApp Msg.py
@@ -1,6 +1,5 @@
 import pywhatkit
 import pyautogui
-"""Now enter the phone with country code, msg, time with hours and mins in 24 hours format, the delay seconds followed by the boolean
-to true/false to close the tab"""
+""" In place of xxxxx enter the phone with country code, then for msg (Enter the text you want to send), 15 is the hour (3 PM) and 15 is the min, 12 is the  seconds delay followed by the boolean to true/false for closing the tab"""
 pywhatkit.sendwhatmsg('xxxxxxx', 'Msg', 15, 15, 12, True, 10)
 pyautogui.press('enter')
